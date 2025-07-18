81b6b45275f923e7de9a559042ef22cd4f5e32f4
kumarsachin88 <SACHINKUMARROY561@GMAIL.COM>
2025-07-18 15:14:14 +0530

    fisrt

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
-import NextAuth from "next-auth/next";
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
+const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -51,8 +51,6 @@ export const authOptions = {
     error: "/components/signin",
   },
   debug: process.env.NODE_ENV === 'development',
-};
-
-const handler = NextAuth(authOptions);
+});
 
 export { handler as GET, handler as POST };
--- src/app/api/cart/clear/route.js
+++ src/app/api/cart/clear/route.js
@@ -1,6 +1,7 @@
 import { connectDB } from '../../../../lib/mongodb';
 import { getServerSession } from 'next-auth/next';
-import { authOptions } from '../../auth/[...nextauth]/route';
+import NextAuth from 'next-auth';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 
 export async function DELETE() {
   try {
