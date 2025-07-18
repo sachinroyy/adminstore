4a76123b0d3831fcb71b747acf693ff6e835319b
kumarsachin88 <SACHINKUMARROY561@GMAIL.COM>
2025-07-18 15:02:32 +0530

    firtst

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
-const handler = NextAuth({
+export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -51,6 +51,8 @@ const handler = NextAuth({
     error: "/components/signin",
   },
   debug: process.env.NODE_ENV === 'development',
-});
+};
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
